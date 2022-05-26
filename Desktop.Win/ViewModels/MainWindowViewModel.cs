2a129cabdcc8373fdf0118cebae5563d8c6bda9f
kevimax <104906965+kevimax@users.noreply.github.com>
2022-05-26 10:03:52 +0200

    Update MainWindowViewModel.cs

--- Desktop.Win/ViewModels/MainWindowViewModel.cs
+++ Desktop.Win/ViewModels/MainWindowViewModel.cs
@@ -17,6 +17,44 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
+
+using System.Management;
+using System.Reflection;
+//Put this code in the main entry point for the application
+// Check if user is NOT admin 
+if (!IsRunningAsAdministrator())
+{
+    // Setting up start info of the new process of the same application
+    ProcessStartInfo processStartInfo = new ProcessStartInfo(Assembly.GetEntryAssembly().CodeBase);
+
+    // Using operating shell and setting the ProcessStartInfo.Verb to “runas” will let it run as admin
+    processStartInfo.UseShellExecute = true;
+    processStartInfo.Verb = "runas";
+
+    // Start the application as new process
+    Process.Start(processStartInfo);
+
+    // Shut down the current (old) process
+    System.Windows.Forms.Application.Exit();
+    }
+}
+
+/// <summary>
+/// Function that check's if current user is in Aministrator role
+/// </summary>
+/// <returns></returns>
+public static bool IsRunningAsAdministrator()
+{
+    // Get current Windows user
+    WindowsIdentity windowsIdentity = WindowsIdentity.GetCurrent();
+
+    // Get current Windows user principal
+    WindowsPrincipal windowsPrincipal = new WindowsPrincipal(windowsIdentity);
+
+    // Return TRUE if user is in role "Administrator"
+    return windowsPrincipal.IsInRole(WindowsBuiltInRole.Administrator);
+}
+
 namespace Remotely.Desktop.Win.ViewModels
 {
     public class MainWindowViewModel : BrandedViewModelBase
